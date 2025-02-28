f93f33f2cba39ac09e7e40e4513d570c97b3efbc
Roaa837 <74208514+Roaa837@users.noreply.github.com>
2025-02-28 22:43:17 +0200

    creating models

---
+++ src/main/java/com/example/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.example.repository;
+
+import jdk.jfr.Registered;
+
+@Registered
+
+public class ProductRepository {
+}
---
+++ src/main/java/com/example/repository/UserRepository.java
@@ -0,0 +1,45 @@
+package com.example.repository;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Repository;
+import com.example.model.User;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.UUID;
+@SuppressWarnings("rawtypes")
+@Repository
+public class UserRepository extends MainRepository<User> {
+    @Override
+    protected String getDataPath() {
+        return "src/main/java/com.example/data/users.json";
+    }
+    @Override
+    protected Class<User[]> getArrayType() {
+        return User[].class;
+    }
+    public Optional<User> findById(String id) {
+        try {
+            return findAll().stream()
+                    .filter(user -> user.getId().equals(id))
+                    .findFirst();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to read users.json");
+        }
+    }
+    public void deleteById(String id) {
+        try {
+        ArrayList<User> users = findAll();
+        users.removeIf(user -> user.getId().toString().equals(id));
+        overrideData(users);}
+        catch (Exception e) {
+            e.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to delete users.json");
+        }
+    }
+
+}
+
+
