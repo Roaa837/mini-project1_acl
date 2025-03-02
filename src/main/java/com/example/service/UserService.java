32350aea958cbb563594a3ff0501fc394d92a386
Roaa837 <74208514+Roaa837@users.noreply.github.com>
2025-03-02 23:20:42 +0200

    UserRepository and controllers

--- src/main/java/com/example/service/UserService.java
+++ src/main/java/com/example/service/UserService.java
@@ -1,8 +1,40 @@
 package com.example.service;
 
+import com.example.model.Order;
+import com.example.model.User;
+import com.example.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@SuppressWarnings("rawtypes")
 @Service
 
-public class UserService {
+public class UserService extends MainService<User> {
+    private final UserRepository userRepository;
+
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    public User addUser(User user) {
+        return userRepository.addUser(user);
+    }
+    public ArrayList<User> getUsers() {
+        return new ArrayList<>(userRepository.findAll());
+    }
+    public User getUserById(UUID userId)
+    {
+        return userRepository.getUserById(userId);
+    }
+    public List<Order> getOrdersByUserId(UUID userId)
+    {
+        return userRepository.getOrdersByUserId(userId);
+    }
+    public void deleteUserById(UUID userId)
+    {
+        userRepository.deleteUserById(userId);
+    }
 }
