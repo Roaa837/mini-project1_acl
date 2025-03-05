0d3682c1ce36f92bf5a4a53b9cd55dc9e913030a
Ahmed Sherif <asherif2002@gmail.com>
2025-03-05 13:23:18 +0200

    DataPath

--- src/main/java/com/example/controller/OrderController.java
+++ src/main/java/com/example/controller/OrderController.java
@@ -1,4 +1,16 @@
 package com.example.controller;
 
+import com.example.model.Order;
+import com.example.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/order")
 public class OrderController {
+
+   
 }
--- src/main/java/com/example/service/OrderService.java
+++ src/main/java/com/example/service/OrderService.java
@@ -1,7 +1,16 @@
 package com.example.service;
 
+import com.example.model.Order;
+import com.example.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
 @Service
-public class OrderService {
+@SuppressWarnings("rawtypes")
+public class OrderService extends MainService<Order> {
+
+
 }
