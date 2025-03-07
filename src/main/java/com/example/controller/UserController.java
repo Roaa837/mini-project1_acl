bac5b6fa765ebf6eeb6666d0ba5ec8cb2f8f9132
Roaa837 <74208514+Roaa837@users.noreply.github.com>
2025-03-07 03:45:20 +0200

    missing one function in the service

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,22 +1,35 @@
 package com.example.controller;
 
+import com.example.model.Cart;
 import com.example.model.Order;
+import com.example.model.Product;
 import com.example.model.User;
+import com.example.service.CartService;
+import com.example.service.ProductService;
 import com.example.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @RestController
+
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
     private final UserService userService;
+    private final ProductService productService;
+    private final CartService cartService;
 
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, ProductService productService, CartService cartService) {
         this.userService = userService;
+        this.productService = productService;
+        this.cartService = cartService;
     }
 
 
@@ -42,7 +55,6 @@ public class UserController {
         return userService.getOrdersByUserId(userId);
     }
 
-
     @DeleteMapping("/delete/{userId}")
     public String deleteUserById(@PathVariable UUID userId) {
         try {
@@ -50,6 +62,65 @@ public class UserController {
             return "User with ID " + userId + " was successfully deleted.";
         } catch (Exception e) {
             return "Error deleting user with ID " + userId + ": " + e.getMessage();
+        }
+    }
+    @PostMapping("/{userId}/removeOrder")
+    public String removeOrderFromUser(@PathVariable UUID userId, @RequestParam UUID orderId)
+    {
+        try{
+            userService.removeOrderFromUser(userId, orderId);
+            return "User with ID " + userId + " was successfully removed.";
+        }catch(Exception e){
+            return "Error deleting user with ID " + userId + ": " + e.getMessage();
+        }
+
+    }
+    @DeleteMapping("/{userId}/emptyCart")
+    public String emptyCart(@PathVariable UUID userId){
+        try{
+            userService.emptyCart(userId);
+            return "User with ID " + userId + " was successfully deleted.";
+        }catch (Exception e){
+            return "Error deleting user with ID " + userId + ": " + e.getMessage();
+        }
+    }
+    @PutMapping("/addProductToCart")
+    public String addProductToCart(@RequestParam UUID userId, @RequestParam UUID productId) {
+        try {
+            Cart cart = cartService.getCartByUserId(userId);
+            if (cart == null) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Cart not found for user");
+            }
+
+            Product product = productService.getProductById(productId);
+            if (product == null) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found");
+            }
+
+            cartService.addProductToCart(cart.getId(), product);
+            return "Product added to cart successfully!";
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to add product to cart");
+        }
+    }
+    @PutMapping("/deleteProductFromCart")
+    public String deleteProductFromCart(@RequestParam UUID userId, @RequestParam UUID productId){
+        try{
+            Cart cart = cartService.getCartByUserId(userId);
+            if (cart == null) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Cart not found for user");
+            }
+            Product product = productService.getProductById(productId);
+            if (product == null) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found");
+            }
+            cartService.deleteProductFromCart(cart.getId(), product);
+            return "Product deleted from cart successfully!";
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to remove product from cart");
         }
     }
 
--- src/main/java/com/example/service/UserService.java
+++ src/main/java/com/example/service/UserService.java
@@ -1,9 +1,14 @@
 package com.example.service;
 
+import com.example.model.Cart;
 import com.example.model.Order;
 import com.example.model.User;
+import com.example.repository.CartRepository;
 import com.example.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,11 +18,16 @@ import java.util.UUID;
 @Service
 
 public class UserService extends MainService<User> {
+    @Autowired
     private final UserRepository userRepository;
+    private  final CartService cartService;
+    @Autowired
+    private CartRepository cartRepository;
 
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, CartService cartService) {
         this.userRepository = userRepository;
+        this.cartService = cartService;
     }
     public User addUser(User user) {
         return userRepository.addUser(user);
@@ -36,5 +46,22 @@ public class UserService extends MainService<User> {
     public void deleteUserById(UUID userId)
     {
         userRepository.deleteUserById(userId);
+    }
+    public void addOrderToUser(UUID userId)
+    {
+        //        i don't fully understand it
+
+    }
+    public void emptyCart(UUID userId)
+    {
+        Cart cart =  cartService.getCartById(userId);
+        if (cart == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Cart not found for this user");
+        }
+        cart.getProducts().clear();
+        cartRepository.overrideData(cartRepository.getCarts());
+    }
+    public void removeOrderFromUser(UUID userId, UUID orderId){
+        userRepository.removeOrderFromUser(userId, orderId);
     }
 }
